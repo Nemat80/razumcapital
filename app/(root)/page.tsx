ac7d00d9e0817ae3a50b8b3c77d2a4fc42f7fc37
dars 1 <dasr1 1>
2023-08-25 01:09:28 +0500

    fix redirect 2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ import { redirect } from "next/navigation";
 export default async function Home() {
 
   const user = await currentUser();
-  if (!user) return redirect("/");
+  if (!user) return redirect("/sign-in");
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
